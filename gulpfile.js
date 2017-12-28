3c9995029b21962e8c190ebca23b7cc5b5e3b774
Buyung A <buyung.abadi@sebangsa.com>
2017-12-28 12:56:24 +0700

    initialize

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var connect = require('gulp-connect');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+var imagemin = require('gulp-imagemin');
+var pug = require('gulp-pug');
+
+const dirs = {
+  src: 'src',
+  dest: 'dist'
+};
+
+const stylesConfig = {
+  src: dirs.src+'/styles/*.scss',
+  dest: dirs.dest+'/css'
+};
+
+const scriptsConfig = {
+  src: dirs.src+'/scripts/*.js',
+  dest: dirs.dest+'/js'
+};
+
+const imagesConfig = {
+  src: dirs.src+'/images/*',
+  dest: dirs.dest+'/images'
+};
+
+const htmlConfig = {
+  src: dirs.src+'/templates/*.pug',
+  dest: dirs.dest
+};
+
+gulp.task('styles', () => {
+  gulp.src(stylesConfig.src)
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(gulp.dest(stylesConfig.dest))
+    .pipe(connect.reload());
+});
+
+gulp.task('scripts', function(){
+    gulp.src(scriptsConfig.src)
+    .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(scriptsConfig.dest))
+    .pipe(connect.reload());
+});
+
+gulp.task('images', function() {
+  gulp.src(imagesConfig.src)
+    .pipe(imagemin())
+    .pipe(gulp.dest(imagesConfig.dest))
+    .pipe(connect.reload());
+});
+
+gulp.task('html', function() {
+  gulp.src(htmlConfig.src)
+  .pipe(pug({
+    pretty:' '
+  }))
+  .pipe(gulp.dest(htmlConfig.dest))
+  .pipe(connect.reload());
+});
+
+gulp.task('server', function () {
+  connect.server({
+    root: ['./dist'],
+    livereload: true,
+    port: 4200,
+  });
+});
+
+gulp.task('watch', function () {
+  gulp.watch(stylesConfig.src, ['styles']);
+  gulp.watch(scriptsConfig.src, ['scripts']);
+  gulp.watch(imagesConfig.src, ['images']);
+  gulp.watch(htmlConfig.src, ['html']);
+});
+
+gulp.task('default', ['scripts', 'styles', 'images', 'html', 'server', 'watch']);
+gulp.task('build', ['scripts', 'styles', 'images', 'html']);
